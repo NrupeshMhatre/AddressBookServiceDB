b39e6aeaf1c372cac773a8a284e6715167521240
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-05-09 10:30:53 +0530

    Insert new Contacts

--- UC1.sql
+++ UC1.sql
@@ -12,3 +12,11 @@ CREATE TABLE AddressBook (
         email varchar(20) NOT NULL UNIQUE
 );
 desc AddressBook;
+
+INSERT INTO addressbook VALUES
+(1,'Suresh','Patil','Khopta','Uran','Maharashtra','424112','1234567890', 'Suresh.m@gmail.com'),
+(2,'Sumit','Khot','Ulwe','NaviMumbai','Maharashtra','420166','9191919291', 'Sumit.k@gmail.com'),
+(3,'Mayu','Mhatre','Uran','NaviMumbai','MH','487879','4567890125', 'Mayu.m@yahoo.com'),
+(4,'Kumar','k','Madhya Pradesh','Gwalior','MP','43342','6577567890', 'kumar.m@gmail.com');
+
+SELECT * FROM addressbook;
