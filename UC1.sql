8a4b130862317d5c5b280541c67a1474e49bd6d7
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-05-09 11:49:22 +0530

    ER Diagram for Address Book

--- UC1.sql
+++ UC1.sql
@@ -39,3 +39,59 @@ SELECT * FROM Addressbook WHERE state = 'Maharashtra' ORDER BY firstname DESC;
 SELECT ab_type, COUNT(*) FROM addressbook GROUP BY ab_type;
 insert into addressbook(id,firstname,lastname,address,city,state,zip,phonenumber,email,ab_name,ab_type)
  values(4,'Viraj', 'Thakur', 'satara', 'satara', 'Maharashtra','424112', '1234567890',	'vir@gmail.com',	'AddressBook2',	'friends');
+ CREATE TABLE addressbooks (
+		id int not null auto_increment primary key,
+		firstname varchar(20) NOT NULL, 
+        lastname varchar(20) NOT NULL,
+        address varchar(100),
+        city varchar(20) NOT NULL,
+        state varchar(20) NOT NULL,
+        zip integer(6),
+        phonenumber varchar(10) NOT NULL,
+        email varchar(20) NOT NULL UNIQUE
+);
+desc addressbooks;
+
+create table addressbooks_name(
+  id int not null auto_increment primary key,
+  name varchar (20) not null,
+  ab_id int ,
+  foreign key (ab_id)references addressbooks(id)  
+);
+desc addressbooks_name;
+
+create table addressbooks_type(
+id int not null primary key,
+type varchar (20)
+);
+desc addressbooks_type;
+
+create table addressbooks_abtype(
+  a_id int not null,
+  t_id int not null,
+  foreign key(a_id) references addressbooks(id),
+  foreign key(t_id) references addressbooks_type(id),
+  primary key(a_id,t_id)
+);
+
+INSERT INTO addressbooks VALUES
+(1,'Sayyam','Gaikwad','Owla','Panvel','Maharashtra','424112','1234567890', 'vishaka.m@gmail.com'),
+(2,'Rohan','Patil','Khopta','Uran','Maharashtra','420166','9191919291', 'kunal.p@gmail.com'),
+(3,'shravani','Khot','panvel','Mh','Maharashtra','487879','4567890125', 'shanvika.k@yahoo.com'),
+(4,'priyesh','Mhatre','Navin Sheva','Uran','Maharashtra','43342','6577567890', 'pihu.m@gmail.com');
+ 
+insert into addressbooks_name values (1,'AdddressBook1', 2); 
+
+insert into addressbooks_type
+values(1,'family');
+insert into addressbooks_type
+values(2,'friends');
+insert into addressbooks_type
+values(3,'profession');
+
+insert into addressbooks_abtype(a_id,t_id)values(1,1);
+
+select * from addressbooks;
+select * from addressbooks_name;
+select * from addressbooks_type;
+select * from addressbooks_abtype;
