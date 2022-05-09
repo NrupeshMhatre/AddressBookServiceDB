10d311e630456eeaa10b8f8ba21972d5822b00c4
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-05-09 10:44:02 +0530

    Delete person

--- UC1.sql
+++ UC1.sql
@@ -13,15 +13,18 @@ CREATE TABLE AddressBook (
 );
 desc AddressBook;
 
-INSERT INTO addressbook VALUES
+INSERT INTO Addressbook VALUES
 (1,'Suresh','Patil','Khopta','Uran','Maharashtra','424112','1234567890', 'Suresh.m@gmail.com'),
 (2,'Sumit','Khot','Ulwe','NaviMumbai','Maharashtra','420166','9191919291', 'Sumit.k@gmail.com'),
 (3,'Mayu','Mhatre','Uran','NaviMumbai','MH','487879','4567890125', 'Mayu.m@yahoo.com'),
 (4,'Kumar','k','Madhya Pradesh','Gwalior','MP','43342','6577567890', 'kumar.m@gmail.com');
 
-SELECT * FROM addressbook;
-UPDATE addressbook SET firstname = 'Suraj'
+SELECT * FROM Addressbook;
+UPDATE Addressbook SET firstname = 'Suraj'
 WHERE id = 1; 
 
-SELECT * from addressbook where firstname = 'Suraj';
-SELECT firstname from addressbook where firstname = 'Suraj';
+SELECT * from Addressbook where firstname = 'Suraj';
+SELECT firstname from Addressbook where firstname = 'Suraj';
+
+DELETE from Addressbook WHERE id =4 ;
+SELECT * FROM Addressbook WHERE firstname = 'kumar';
